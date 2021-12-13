ec4ebfe46756bcd51a7649c61bd6e9ac1b5b30ca
Gabriel Falcão <gabriel@nacaolivre.org>
2021-12-13 05:00:16 +0100

    move terraform stuff under terraform

---
+++ terraform/kubernetes_cert_manager.tf
@@ -0,0 +1,6 @@
+# module "cert_manager" {
+#   source        = "terraform-iaac/cert-manager/kubernetes"
+#   cluster_issuer_email                   = "gabrielfalcao@protonmail.com"
+#   cluster_issuer_name                    = "cert-manager-gfalcao-issuer"
+#   cluster_issuer_private_key_secret_name = "cert-manager-gfalcao-private-key"
+# }
