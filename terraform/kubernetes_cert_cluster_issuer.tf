bb56b2214379477537db7900b463fd9a98357fdb
Gabriel Falcão <gabriel@nacaolivre.org>
2021-12-18 23:01:51 +0100

    terraform: prod letencrypt cluster issuer

---
+++ terraform/kubernetes_cert_cluster_issuer.tf
@@ -0,0 +1,24 @@
+resource "kubectl_manifest" "cert_cluster_issuer_staging" {
+    yaml_body = <<YAML
+apiVersion: cert-manager.io/v1alpha2
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-prod
+  namespace: cert-manager
+spec:
+  acme:
+    # The ACME server URL
+    server: https://acme-v02.api.letsencrypt.org/directory
+    # Email address used for ACME registration
+    email: gabrielfalcao@protonmail.com
+    # Name of a secret used to store the ACME account private key
+    privateKeySecretRef:
+      name: letsencrypt-prod
+    # Enable the HTTP-01 challenge provider
+    solvers:
+    - http01:
+        ingress:
+          class: nginx
+
+YAML
+}
